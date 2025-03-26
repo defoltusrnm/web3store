ad90401006e63bc6bc6d04e40b6d186531e54ba7
defoltusrnm <defoltusrnm@gmail.com>
2025-03-26 15:38:40 +0100

    dadwada

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ pub mod utils;
 extern crate axum;
 use std::time::Duration;
 
-use axum::{Json, Router, routing::post};
+use axum::{Json, Router, response::Result, routing::post};
 use http::StatusCode;
 use keycloak::services::{
     authorization_implementation::DefaultAdminTokenProvider,
@@ -12,7 +12,12 @@ use keycloak::services::{
     host_implementation::EnvHostAddressProvider,
     management::KeycloakManagement,
     management_implementation::DefaultKeycloakManagement,
-    requests::create_user::CreateUserRequest,
+    queries::{clients::ClientsQuery, role::RoleQuery, users::UsersQuery},
+    requests::{
+        assign_roles::{AssignRoleRequest, AssignRolesRequest},
+        create_user::CreateUserRequest,
+        update_users_email_request::UpdateUsersEmailRequest,
+    },
     routes_implementation::DefaultAdminRoutes,
     seeding::{KeycloakSeeding, KeycloakSeedingArguments},
     seeding_implementation::DefaultKeycloakSeeding,
@@ -21,7 +26,12 @@ use keycloak::services::{
 };
 use serde::Deserialize;
 use tokio_util::sync::CancellationToken;
-use utils::{dotenv::configure_dotenv, env::env_var, errors::AppErr, logging::configure_logs};
+use utils::{
+    dotenv::configure_dotenv,
+    env::env_var,
+    errors::{AppErr, HttpAppErr},
+    logging::configure_logs,
+};
 
 #[tokio::main]
 async fn main() -> Result<(), AppErr> {
@@ -76,7 +86,7 @@ fn create_customer_router() -> Router {
     Router::new().route("/api/customers", post(create_customer))
 }
 
-async fn create_customer(Json(request): Json<CreateCustomerRequest>) -> StatusCode {
+async fn create_customer(Json(request): Json<CreateCustomerRequest>) -> Result<StatusCode> {
     let host_provider = &EnvHostAddressProvider::new("KEYCLOAK_HOST");
     let credentials_provider =
         &EnvAdminCredentialProvider::new("KEYCLOAK_ADMIN_LOGIN", "KEYCLOAK_ADMIN_PASSWORD");
@@ -84,19 +94,88 @@ async fn create_customer(Json(request): Json<CreateCustomerRequest>) -> StatusCo
     let auth_provider = &DefaultAdminTokenProvider::new(routes, credentials_provider);
     let manager = &DefaultKeycloakManagement::new(auth_provider, routes);
 
-    match env_var("KEYCLOAK_REALM") {
-        Ok(realm_name) => match manager
-            .create_user(
-                &CreateUserRequest::new(&realm_name, &request.email, &request.password),
-                &CancellationToken::new(),
-            )
-            .await
-        {
-            Ok(_) => StatusCode::CREATED,
-            Err(_) => StatusCode::BAD_REQUEST,
-        },
-        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
-    }
+    let realm_name = env_var("KEYCLOAK_REALM")
+        .map_err(|_| HttpAppErr::new(StatusCode::INTERNAL_SERVER_ERROR, "Error"))?;
+
+    let client_name = env_var("KEYCLOAK_CLIENT")
+        .map_err(|_| HttpAppErr::new(StatusCode::INTERNAL_SERVER_ERROR, "Error"))?;
+
+    let role_name = env_var("KEYCLOAK_CUSTOMER_ROLE")
+        .map_err(|_| HttpAppErr::new(StatusCode::INTERNAL_SERVER_ERROR, "Error"))?;
+
+    let clients = manager
+        .query_clients(
+            &ClientsQuery::new(&realm_name, &client_name),
+            &CancellationToken::new(),
+        )
+        .await
+        .inspect_err(|err| log::error!("failed to retrieve client: {err}"))
+        .map_err(|_| HttpAppErr::new(StatusCode::FAILED_DEPENDENCY, "keycloak error"))?;
+
+    let client = clients
+        .get(0)
+        .map(Result::Ok)
+        .unwrap_or(Err(HttpAppErr::new(
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "failed to create user",
+        )))?;
+
+    let role = manager
+        .query_role(
+            &RoleQuery::new(&realm_name, &client.id, &role_name),
+            &CancellationToken::new(),
+        )
+        .await
+        .inspect_err(|err| log::error!("failed to retrieve role: {err}"))
+        .map_err(|_| HttpAppErr::new(StatusCode::FAILED_DEPENDENCY, "keycloak error"))?;
+
+    manager
+        .create_user(
+            &CreateUserRequest::new(&realm_name, &request.email, &request.password),
+            &CancellationToken::new(),
+        )
+        .await
+        .inspect_err(|err| log::error!("failed to create user: {err}"))
+        .map_err(|_| HttpAppErr::new(StatusCode::FAILED_DEPENDENCY, "keycloak error"))?;
+
+    let users = manager
+        .query_users(
+            &UsersQuery::new(&realm_name, &request.email),
+            &CancellationToken::new(),
+        )
+        .await
+        .inspect_err(|err| log::error!("failed to retrieve user: {err}"))
+        .map_err(|_| HttpAppErr::new(StatusCode::FAILED_DEPENDENCY, "keycloak error"))?;
+
+    let user = users.get(0).map(Result::Ok).unwrap_or(Err(HttpAppErr::new(
+        StatusCode::INTERNAL_SERVER_ERROR,
+        "failed to create user",
+    )))?;
+
+    manager
+        .update_users_email(
+            &UpdateUsersEmailRequest::new_verified(&realm_name, &user.id, &request.email),
+            &CancellationToken::new(),
+        )
+        .await
+        .inspect_err(|err| log::error!("failed to update user email: {err}"))
+        .map_err(|_| HttpAppErr::new(StatusCode::FAILED_DEPENDENCY, "keycloak error"))?;
+
+    manager
+        .assign_roles(
+            &AssignRolesRequest::new(
+                &realm_name,
+                &user.id,
+                &client.id,
+                &[AssignRoleRequest::new(&role.id, &role.name)],
+            ),
+            &CancellationToken::new(),
+        )
+        .await
+        .inspect_err(|err| log::error!("failed to update user email: {err}"))
+        .map_err(|_| HttpAppErr::new(StatusCode::FAILED_DEPENDENCY, "keycloak error"))?;
+
+    Ok(StatusCode::CREATED)
 }
 
 #[derive(Deserialize)]
--- src/utils/errors.rs
+++ src/utils/errors.rs
@@ -1,9 +1,11 @@
+use axum::{Json, response::IntoResponse};
 use derive_more::Display;
-
+use http::StatusCode;
+use serde::Serialize;
 
 #[derive(Display, Debug)]
 pub struct AppErr {
-    msg: String
+    msg: String,
 }
 
 impl AppErr {
@@ -11,6 +13,48 @@ impl AppErr {
         AppErr { msg }
     }
     pub fn from(msg: &str) -> Self {
-        AppErr { msg: msg.to_owned() }
+        AppErr {
+            msg: msg.to_owned(),
+        }
     }
+}
+
+pub struct HttpAppErr {
+    pub status: StatusCode,
+    pub reason: String,
+}
+
+impl HttpAppErr {
+    pub fn new(status: StatusCode, reason: &str) -> Self {
+        HttpAppErr {
+            status,
+            reason: reason.to_owned(),
+        }
+    }
+
+    pub fn from(status: StatusCode, app_err: AppErr) -> Self {
+        HttpAppErr {
+            status,
+            reason: app_err.msg,
+        }
+    }
+}
+
+impl IntoResponse for HttpAppErr {
+    fn into_response(self) -> axum::response::Response {
+        let error_msg = HttpErrorMessage {
+            title: self.status.as_str().to_owned(),
+            status: self.status.as_u16(),
+            reason: self.reason,
+        };
+
+        (self.status, Json(error_msg)).into_response()
+    }
+}
+
+#[derive(Serialize)]
+struct HttpErrorMessage {
+    pub title: String,
+    pub status: u16,
+    pub reason: String,
 }
