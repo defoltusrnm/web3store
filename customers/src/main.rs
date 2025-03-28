7af9cf2a2ee3b66acea046def0f2043daac1df9b
defoltusrnm <defoltusrnm@gmail.com>
2025-03-28 13:08:09 +0100

    add consumer

--- customers/src/main.rs
+++ customers/src/main.rs
@@ -1,6 +1,46 @@
+use futures::StreamExt;
+use rdkafka::{
+    ClientConfig, Message,
+    consumer::{Consumer, StreamConsumer},
+};
+use utils::{dotenv::configure_dotenv, env::env_var, errors::AppErr, logging::configure_logs};
+
 #[tokio::main]
-async fn main() -> Result<(), ()> {
-    println!("Hello, world!");
+async fn main() -> Result<(), AppErr> {
+    configure_dotenv();
+    _ = configure_logs(log::LevelFilter::Info)?;
+
+    consume_topic().await?;
+
+    Ok(())
+}
+
+async fn consume_topic() -> Result<(), AppErr> {
+    let kafka_host = env_var("KAFKA_HOST")?;
+    let kafka_topic = &env_var("KAFKA_CUSTOMER_TOPIC")?;
+
+    let consumer: StreamConsumer = ClientConfig::new()
+        .set("bootstrap.servers", kafka_host)
+        .set("group.id", "solution_group")
+        .set("enable.auto.commit", "true")
+        .set("auto.offset.reset", "earliest")
+        .create()
+        .map_err(|err| AppErr::from_owned(format!("failed to start consumer: {err}")))?;
+
+    consumer
+        .subscribe(&[kafka_topic])
+        .map_err(|err| AppErr::from_owned(format!("failed to start consumer: {err}")))?;
+
+    while let Some(message) = consumer.stream().next().await {
+        match message {
+            Ok(m) => {
+                if let Some(payload) = m.payload() {
+                    log::info!("Received message: {}", String::from_utf8_lossy(payload));
+                }
+            }
+            Err(e) => log::error!("Kafka error: {}", e),
+        }
+    }
 
     Ok(())
 }
