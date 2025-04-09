1b2e3d6c7b72401b213fc19740e1f1ed8e8e8252
defoltusrnm <defoltusrnm@gmail.com>
2025-04-09 22:09:43 +0300

    fixes

--- ui/src/utils/theming/Center.tsx
+++ ui/src/utils/theming/Center.tsx
@@ -2,7 +2,7 @@
 export const Center: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
         <div className="relative h-full bg-inherit">
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-blue-300">
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                 {children}
             </div>
         </div>
